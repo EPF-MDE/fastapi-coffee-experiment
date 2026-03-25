6f6914dfe04e6b1101bfbe7e5412197929eee571
Xavier <xavier.cazalot@gmail.com>
2026-03-25 12:35:12 +0100

    FastAPI : Create operation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+coffees = []
+
 
 class Coffee(BaseModel):
     name: str
@@ -18,6 +20,17 @@ def read_home():
 @app.get("/coffees/{coffee_id}")
 def read_coffee(coffee_id: int, q: str | None = None):
     return {"coffee_id": coffee_id, "q": q}
+
+
+@app.post("/coffees")
+def create_coffee(coffee_name: str, price: float, is_offer: bool | None):
+    coffee = {"name": coffee_name, "price": price, "is_offer": is_offer}
+
+    coffees.append(coffee)
+
+    response = {"coffee_id": len(coffees) - 1, "coffee": coffee}
+
+    return response
 
 
 @app.put("/coffees/{coffee_id}")
