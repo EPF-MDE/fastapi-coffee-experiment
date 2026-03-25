13a1223c30028e2b7eb33c970fab82e03b6a4d10
Xavier <xavier.cazalot@gmail.com>
2026-03-25 12:36:21 +0100

    Read from the in-memory DB

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def read_home():
 
 
 @app.get("/coffees/{coffee_id}")
-def read_coffee(coffee_id: int, q: str | None = None):
-    return {"coffee_id": coffee_id, "q": q}
+def read_coffee(coffee_id: int, quantity: int | None = None):
+    return {"coffee_id": coffee_id, "coffee": coffees[coffee_id], "quantity": quantity}
 
 
 @app.post("/coffees")
